8a5ec1e08705e3aacede33784526f90cd318db29
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-05-11 17:02:37 -0300

    adicionando box de preco

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -2,10 +2,14 @@ import {
     Box,
     Button,
     Container,
+    FormControl,
     IconButton,
+    InputLabel,
+    InputAdornment,
     Select,
     TextField,
-    Typography
+    Typography,
+    OutlinedInput,
 }from '@material-ui/core'
 
 import { useDropzone } from 'react-dropzone'
@@ -216,6 +220,23 @@ const Publish = () => {
                         variant="outlined"
                         fullWidth
                     />
+                </Box>
+            </Container>
+                        
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box className={classes.box}>
+                    <Typography component="h6" variant="h6" color="textPrimary">
+                            Preço
+                    </Typography>
+                    <br />
+                    <FormControl fullWidth variant="outlined">
+                        <InputLabel>Valor</InputLabel>
+                        <OutlinedInput 
+                            onChange={() => {}}
+                            startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+                            labelWidth={40}
+                        />
+                    </FormControl>
                 </Box>
             </Container>
 

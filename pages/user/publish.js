01c1d84eaa8b0365a864c29eb813e67b68d7fcf3
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-05-11 15:55:09 -0300

    adicionando o dropzone no upload de imagens

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,9 +1,20 @@
-import { Box, Button, Container, IconButton, Select, TextField, Typography } from '@material-ui/core'
+import {
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Select,
+    TextField,
+    Typography
+}from '@material-ui/core'
+
+import { useDropzone } from 'react-dropzone'
 import { makeStyles } from '@material-ui/core/styles'
 import { DeleteForever, NoEncryption } from '@material-ui/icons'
 
 
 import TemplateDefault from '../../src/templates/Default'
+import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
     mask: {},
@@ -20,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
     },
     thumbsContainer: {
         display: 'flex',
+        flexWrap: 'wrap',
         marginTop: 15,
     },
     dropzone: {
@@ -39,6 +51,7 @@ const useStyles = makeStyles((theme) => ({
         width: 200,
         height: 150,
         backgroundSize: 'cover',
+        margin: '0 15px 15px 0',
         backgroundPosition: 'center center',
         
         '& $mainImage': {
@@ -68,6 +81,23 @@ const useStyles = makeStyles((theme) => ({
 
 const Publish = () => {
     const classes = useStyles()
+    const [files, setFiles] = useState([])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: (acceptedFile) => {
+            const newFiles = acceptedFile.map(file => {
+                return Object.assign(file, {
+                    preview: URL. createObjectURL(file)
+                })
+            })
+
+            setFiles([
+                ...files,
+                ...newFiles,
+            ])
+        }
+    })
 
     return (
         <TemplateDefault>
@@ -132,27 +162,37 @@ const Publish = () => {
                         A primeira imagem é a foto principal do seu anúncio.
                     </Typography>
                     <Box className={classes.thumbsContainer}>
-                        <Box className={classes.dropzone}>
+                        <Box className={classes.dropzone} {...getRootProps()}>
+                            <input {...getInputProps()} />
                             <Typography variant="body2" color="textPrimary">
                                 Clique para adicionar ou arraste a imagem para aqui.
                             </Typography>
                         </Box>
 
-                        <Box 
-                            className={classes.thumb}
-                            style={{backgroundImage: 'url(http://source.unsplash.com/random)'}}
-                        >
-                            <Box className={classes.mainImage}>
-                                <Typography variant="body" color="secondary">
-                                    Principal
-                                </Typography>
-                            </Box>
-                            <Box className={classes.mask}>
-                                <IconButton color="secondary">
-                                    <DeleteForever fontSize="large" />
-                                </IconButton>
-                            </Box>
-                        </Box>
+                        {
+                            files.map((file, index) => (
+                                <Box 
+                                    key={file.name}
+                                    className={classes.thumb}
+                                    style={{backgroundImage:`url(${file.preview})`}}
+                                    >
+                                    {
+                                        index === 0 ?
+                                            <Box className={classes.mainImage}>
+                                                <Typography variant="body" color="secondary">
+                                                    Principal
+                                                </Typography>
+                                            </Box>
+                                        : null
+                                    }
+                                    <Box className={classes.mask}>
+                                        <IconButton color="secondary">
+                                            <DeleteForever fontSize="large" />
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            ))
+                        }
                     </Box>
                 </Box>
             </Container>

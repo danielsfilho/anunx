6f98c8446e9421aad7385e69aaa55062f0b1ff29
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-05-21 17:19:31 -0300

    adicionando login com Google

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import { Formik } from 'formik'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { signIn, useSession } from 'next-auth/client'
 
 import {
+    Box,
     Button,
     CircularProgress,
     Container,
@@ -29,6 +31,12 @@ const Signin = () => {
 
     console.log(session)
 
+    const handleGoogleLogin = () => {
+        signIn('google', {
+            callbackUrl: 'http://localhost:3000/user/dashboard'
+        })
+    }
+
     const handleFormSubmit = async values => {
         signIn('credentials',{
             email: values.email,
@@ -46,6 +54,29 @@ const Signin = () => {
             </Container>
 
             <Container maxWidth="md" className={classes.formContainer}>
+
+                <Box display="flex" justifyContent="center">
+                    <Button 
+                        variant="contained"
+                        color="primary"
+                        startIcon={
+                            <Image 
+                                src="/images/logo_google.png"
+                                width={20}
+                                height={20}
+                                alt="Login com Google"
+                            />
+                        }
+                        onClick={handleGoogleLogin}
+                    >
+                        Entrar com Google
+                    </Button>
+                </Box>
+
+                <Box className={classes.orSeparator}>
+                    <span>ou</span>
+                </Box>
+
                 <Formik
                     initialValues={initialValues}
                     validationSchema={validationSchema}

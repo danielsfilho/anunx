f989f74cef1df7b8f67053fd5bdc3a284793e64c
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-06-09 15:43:05 -0300

    salvando dados do anuncio

--- pages/user/publish/index.js
+++ pages/user/publish/index.js
@@ -1,4 +1,7 @@
 import { Formik } from 'formik'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { getSession } from 'next-auth/client'
 
 import {
     Box,
@@ -12,25 +15,70 @@ import {
     Typography,
     MenuItem,
     Input,
+    CircularProgress,
 }from '@material-ui/core'
 
 import TemplateDefault from '../../../src/templates/Default'
-import { initialValues, validationSchema } from './formValues'
 import FileUpload from '../../../src/components/FileUpload'
+import useToasty from '../../../src/contexts/Toasty'
+import { initialValues, validationSchema } from './formValues'
 
 import useStyles from './styles'
 
-const Publish = () => {
+const Publish = ({ userId, image }) => {
     const classes = useStyles()
+    const { setToasty } = useToasty()
+    const router = useRouter()
+
+    const formValues = {
+        ...initialValues,
+    }
+
+    formValues.userId = userId
+    formValues.image = image
+
+    const handleSuccess = () => {
+        setToasty({
+            open: true,
+            text: 'Anúncio cadastrado com sucesso.',
+            severity: 'success'
+        })
+
+        router.push('/user/dashboard')
+    }
+
+    const handleError = () => {
+        setToasty({
+            open: true,
+            text: 'Ops, ocorreu um erro, tente novamente.',
+            severity: 'error'
+        })
+    }
+
+    const handleSubmit = (values) => {
+        const formData = new FormData()
+
+        for (let field in values) {
+            if(field === 'files') {
+                values.files.forEach(file => {
+                    formData.append('files', file)
+                })
+            }else {
+                formData.append(field, values[field])
+            }
+        }
+
+        axios.post('/api/products', formData)
+            .then(handleSuccess)
+            .catch(handleError)
+    }
 
     return (
         <TemplateDefault>
             <Formik
-                initialValues={initialValues}
+                initialValues={formValues}
                 validationSchema={validationSchema}
-                onSubmit={(values) => {
-                    console.log("Ok, enviou o form!", values)
-                }}
+                onSubmit={handleSubmit}
             >
                 {
                     ({
@@ -40,10 +88,14 @@ const Publish = () => {
                         handleChange,
                         handleSubmit,
                         setFieldValue,
+                        isSubmitting,
                     }) => {
                     
                         return(
                             <form onSubmit={handleSubmit}>
+                                <Input type="hidden" name="userId" value={values.userId} />
+                                <Input type="hidden" name="image" value={values.image} />
+
                                 <Container maxWidth="sm">
                                     <Typography component="h1" variant="h2" align="center" color="textPrimary">
                                         Publicar Anúncio
@@ -223,9 +275,11 @@ const Publish = () => {
 
                                 <Container maxWidth="md" className={classes.boxContainer}>
                                     <Box textAlign="right">
-                                        <Button type="submit" variant="contained" color="primary">
-                                            Publicar Anúncio
-                                        </Button>
+                                        {
+                                            isSubmitting
+                                                ? <CircularProgress className={classes.loading} />
+                                                : <Button type="submit" variant="contained" color="primary">Publicar Anúncio</Button> 
+                                        }
                                     </Box>
                                 </Container>
                             </form>
@@ -240,5 +294,16 @@ const Publish = () => {
 }
 
 Publish.requireAuth = true
+
+export async function getServerSideProps({ req }) {
+    const { userId, user } = await getSession({ req })
+
+    return {
+        props: {
+            userId: userId || null,
+            image: user.image
+    }
+}
+}
 
 export default Publish
--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -15,13 +15,11 @@ const FileUpload = ({ files, errors, touched, setFieldValue }) => {
     const { getRootProps, getInputProps } = useDropzone({
         accept: 'image/*',
         onDrop: (acceptedFile) => {
-            const newFiles = acceptedFile.map(file => {
-                return Object.assign(file, {
-                    preview: URL. createObjectURL(file)
-                })
-            })
+            const newFiles = acceptedFile.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            }))
 
-            setFieldValue('files',[
+            setFieldValue('files', [
                 ...files,
                 ...newFiles,
             ])
@@ -37,50 +35,50 @@ const FileUpload = ({ files, errors, touched, setFieldValue }) => {
 
     return(
         <>
-                   <Typography component="h6" variant="h6" color={errors && touched ? "error" : "textPrimary"}>
-                                            Imagens
-                                        </Typography>
-                                        <Typography component="div" variant="body2" color={errors && touched ? "error" : "textPrimary"}>
-                                            A primeira imagem é a foto principal do seu anúncio.
-                                        </Typography>
-                                        {
-                                            errors &&  touched
-                                                ? <Typography variante="body2" color="error" gutterBottom>{errors}</Typography>
-                                                : null
-                                        }
-                                        <Box className={classes.thumbsContainer}>
-                                            <Box className={classes.dropzone} {...getRootProps()}>
-                                                <input name="files" {...getInputProps()} />
-                                                <Typography variant="body2" color={errors && touched ? "error" : "textPrimary"}>
-                                                    Clique para adicionar ou arraste a imagem para aqui.
-                                                </Typography>
-                                            </Box>
+            <Typography component="h6" variant="h6" color={errors && touched ? "error" : "textPrimary"}>
+                Imagens
+            </Typography>
+            <Typography component="div" variant="body2" color={errors && touched ? "error" : "textPrimary"}>
+                A primeira imagem é a foto principal do seu anúncio.
+            </Typography>
+            {
+                errors &&  touched
+                    ? <Typography variante="body2" color="error" gutterBottom>{errors}</Typography>
+                    : null
+            }
+            <Box className={classes.thumbsContainer}>
+                <Box className={classes.dropzone} {...getRootProps()}>
+                    <input name="files" {...getInputProps()} />
+                    <Typography variant="body2" color={errors && touched ? "error" : "textPrimary"}>
+                        Clique para adicionar ou arraste a imagem para aqui.
+                    </Typography>
+                </Box>
 
-                                            {
-                                                files.map((file, index) => (
-                                                    <Box 
-                                                        key={file.name}
-                                                        className={classes.thumb}
-                                                        style={{backgroundImage:`url(${file.preview})`}}
-                                                        >
-                                                        {
-                                                            index === 0 ?
-                                                                <Box className={classes.mainImage}>
-                                                                    <Typography variant="body" color="secondary">
-                                                                        Principal
-                                                                    </Typography>
-                                                                </Box>
-                                                            : null
-                                                        }
-                                                        <Box className={classes.mask}>
-                                                            <IconButton color="secondary" onClick={() => handleRemoveFile(file.path)}>
-                                                                <DeleteForever fontSize="large" />
-                                                            </IconButton>
-                                                        </Box>
-                                                    </Box>
-                                                ))
-                                            }
-                                        </Box>
+                {
+                    files.map((file, index) => (
+                        <Box 
+                            key={file.name}
+                            className={classes.thumb}
+                            style={{backgroundImage:`url(${file.preview})`}}
+                            >
+                            {
+                                index === 0 ?
+                                    <Box className={classes.mainImage}>
+                                        <Typography variant="body" color="secondary">
+                                            Principal
+                                        </Typography>
+                                    </Box>
+                                : null
+                            }
+                            <Box className={classes.mask}>
+                                <IconButton color="secondary" onClick={() => handleRemoveFile(file.path)}>
+                                    <DeleteForever fontSize="large" />
+                                </IconButton>
+                            </Box>
+                        </Box>
+                    ))
+                }
+            </Box>
         </>
     )
 }
